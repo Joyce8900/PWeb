b17a8849d803ce0e9c155d65c4ff03549fb2f6f2
Joyce <joycesantos8900@gmail.com>
2024-11-26 13:54:46 -0300

    add: criação de tabela

--- next0/src/app/page.js
+++ next0/src/app/page.js
@@ -3,6 +3,7 @@ import Button from "./Button";
 export default function Home() {
   return (
     <div className="main">
+      
       <h1> Viva Santana</h1>
       <Button/>
       <h4>Boas Festas</h4>
