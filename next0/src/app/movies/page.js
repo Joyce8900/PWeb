84db27e47719dd76d5d801388f8a90df792d5c38
Joyce <joycesantos8900@gmail.com>
2025-01-08 14:12:11 -0300

    receita concluida

--- next0/src/app/movies/page.js
+++ next0/src/app/movies/page.js
@@ -1,28 +1,25 @@
 import React from "react"
 
 const  page = async({searchParams}) => {
- const { titleSearchKey = "bagdad" } = await searchParams
+ const { titleSearchKey = "Batman", yearSearchKey = "2018" } = await searchParams
+//  https://www.omdbapi.com/?apikey=f1cbc41e&t=Batman&y=2018
 
   const res = await fetch(
-    `http://www.omdbapi.com/?apikey=f1cbc41e&s=${titleSearchKey}`
+    `https://www.omdbapi.com/?apikey=f1cbc41e&t=${titleSearchKey}&y=${yearSearchKey}`
   )
 
     const data = await res.json()
 
-
+    console.log(data)
 
     return (
-
+      <div>
         <div>
-
-            <div>
-
-            {data.Search.map( (m) => <div key={m.imdbID}>{m.Title} --- {m.Year}</div>  )}               
-
-            </div>
-
+          <h1>{data.Title}</h1>
+          <h2>{data.Year}</h2>
+          <img src={data.Poster} alt={data.Title} />
         </div>
-
+      </div>
     )
 }
 
